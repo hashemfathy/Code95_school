5dc3e6332fcee6fb299584d41f9f266bdb7626e3
hashem fathy <hashempoppo23@gmail.com>
2019-08-25 12:39:14 +0200

    update style

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
 	<!-- Custom Stylesheet -->
-    <link rel="stylesheet" href="{{URL::to('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     @yield('style')
 </head>
 <body>
@@ -26,7 +26,7 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="{{URL::to('js/app.js')}}"></script>
+<script src="{{asset('js/app.js')}}"></script>
 
 </body>
 </html>
